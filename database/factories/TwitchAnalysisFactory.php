cc077f72940ebcb1e46a28afcba47585489dc309
egeakat <egeakat@hotmail.com>
2021-06-18 19:14:04 +0300

    initialize commit

---
+++ database/factories/TwitchAnalysisFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\TwitchAnalysis;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class TwitchAnalysisFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = TwitchAnalysis::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
